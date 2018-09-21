c80443b9889cf9ae72a9ad552b98d8148bfa4683
Oskar Gunther <o.gunther@consultia.pl>
2018-09-21 13:51:02 +0200

    Fixes

--- src/MailerBundle/MailerBundle.php
+++ src/MailerBundle/MailerBundle.php
@@ -2,13 +2,13 @@
 
 namespace Mailer\MailerBundle;
 
-use Mailer\MailerBundle\DependencyInjection\MailerExtension;
+use Mailer\MailerBundle\DependencyInjection\ConsultiaMailerExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MailerBundle extends Bundle
 {
     public function getContainerExtension()
     {
-        return new MailerExtension();
+        return new ConsultiaMailerExtension();
     }
 }
